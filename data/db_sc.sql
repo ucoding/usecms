4948408d2151d42ef2b2a1ce8866eb42c21f1481
benbear0519 <benbear0519@gmail.com>
2013-05-16 01:40:34 +0800

    数据库结构和数据分别导出

---
+++ data/db_sc.sql
@@ -0,0 +1,455 @@
+/*
+SQLyog Ultimate v9.10 
+MySQL - 5.5.24-log : Database - duxcms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `dc_admin` */
+
+DROP TABLE IF EXISTS `dc_admin`;
+
+CREATE TABLE `dc_admin` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `gid` int(4) NOT NULL DEFAULT '1',
+  `user` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `nicename` varchar(20) DEFAULT NULL,
+  `regtime` int(4) DEFAULT NULL,
+  `logintime` int(4) DEFAULT NULL,
+  `ip` varchar(15) DEFAULT '未知',
+  `status` int(1) unsigned DEFAULT '1',
+  `loginnum` int(4) DEFAULT '1',
+  `keep` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='管理员信息表';
+
+/*Table structure for table `dc_admin_group` */
+
+DROP TABLE IF EXISTS `dc_admin_group`;
+
+CREATE TABLE `dc_admin_group` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `model_power` text,
+  `class_power` text,
+  `status_power` tinyint(1) NOT NULL DEFAULT '0',
+  `grade` tinyint(1) DEFAULT '3',
+  `keep` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `power_value` (`model_power`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_admin_menu` */
+
+DROP TABLE IF EXISTS `dc_admin_menu`;
+
+CREATE TABLE `dc_admin_menu` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `pid` int(4) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `module` varchar(250) DEFAULT NULL,
+  `status` int(10) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `module` (`module`)
+) ENGINE=MyISAM AUTO_INCREMENT=237 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_category` */
+
+DROP TABLE IF EXISTS `dc_category`;
+
+CREATE TABLE `dc_category` (
+  `cid` int(10) NOT NULL AUTO_INCREMENT,
+  `pid` int(10) NOT NULL DEFAULT '0',
+  `mid` int(10) NOT NULL DEFAULT '1',
+  `sequence` int(10) NOT NULL DEFAULT '0',
+  `show` int(10) NOT NULL DEFAULT '1',
+  `type` int(11) NOT NULL DEFAULT '1',
+  `name` varchar(250) NOT NULL,
+  `urlname` varchar(250) NOT NULL,
+  `image` varchar(250) DEFAULT NULL,
+  `class_tpl` varchar(250) DEFAULT NULL,
+  `content_tpl` varchar(250) DEFAULT NULL,
+  `page` int(10) DEFAULT NULL,
+  `keywords` varchar(250) DEFAULT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `seo_content` text,
+  `lang` int(10) NOT NULL DEFAULT '1',
+  `expand` int(10) DEFAULT NULL,
+  PRIMARY KEY (`cid`),
+  UNIQUE KEY `urlname` (`urlname`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_category_jump` */
+
+DROP TABLE IF EXISTS `dc_category_jump`;
+
+CREATE TABLE `dc_category_jump` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `cid` int(11) unsigned NOT NULL DEFAULT '0',
+  `url` varchar(250) DEFAULT NULL COMMENT '内容',
+  PRIMARY KEY (`id`),
+  KEY `cid` (`cid`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='文章栏目分类';
+
+/*Table structure for table `dc_category_page` */
+
+DROP TABLE IF EXISTS `dc_category_page`;
+
+CREATE TABLE `dc_category_page` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `cid` int(11) unsigned NOT NULL DEFAULT '0',
+  `content` mediumtext COMMENT '内容',
+  PRIMARY KEY (`id`),
+  KEY `cid` (`cid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章栏目分类';
+
+/*Table structure for table `dc_content` */
+
+DROP TABLE IF EXISTS `dc_content`;
+
+CREATE TABLE `dc_content` (
+  `aid` int(10) NOT NULL AUTO_INCREMENT COMMENT '文章ID',
+  `cid` int(10) NOT NULL COMMENT '栏目ID',
+  `title` varchar(250) NOT NULL COMMENT '标题',
+  `urltitle` varchar(250) NOT NULL COMMENT 'URL路径',
+  `keywords` varchar(250) DEFAULT NULL COMMENT '关键词',
+  `description` varchar(250) DEFAULT NULL COMMENT '描述',
+  `updatetime` int(10) DEFAULT NULL COMMENT '更新时间',
+  `inputtime` int(10) NOT NULL COMMENT '发布时间',
+  `image` varchar(250) DEFAULT NULL COMMENT '封面图',
+  `url` varchar(250) DEFAULT NULL COMMENT '跳转',
+  `sequence` int(10) DEFAULT NULL COMMENT '排序',
+  `tpl` varchar(250) DEFAULT NULL COMMENT '模板',
+  `status` int(10) DEFAULT NULL COMMENT '状态',
+  `copyfrom` varchar(250) DEFAULT NULL COMMENT '来源',
+  `views` int(10) NOT NULL DEFAULT '0' COMMENT '浏览数',
+  `position` varchar(250) DEFAULT NULL,
+  `taglink` int(10) NOT NULL DEFAULT '0' COMMENT 'TAG链接',
+  PRIMARY KEY (`aid`),
+  UNIQUE KEY `urltitle` (`urltitle`) USING BTREE,
+  KEY `title` (`title`) USING BTREE,
+  KEY `description` (`copyfrom`)
+) ENGINE=MyISAM AUTO_INCREMENT=10196 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_content_data` */
+
+DROP TABLE IF EXISTS `dc_content_data`;
+
+CREATE TABLE `dc_content_data` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `aid` int(10) NOT NULL,
+  `content` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10196 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_expand_content_chanpin` */
+
+DROP TABLE IF EXISTS `dc_expand_content_chanpin`;
+
+CREATE TABLE `dc_expand_content_chanpin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `aid` int(10) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `pattern` varchar(250) DEFAULT NULL,
+  `listpic` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_expand_content_images` */
+
+DROP TABLE IF EXISTS `dc_expand_content_images`;
+
+CREATE TABLE `dc_expand_content_images` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `aid` int(10) DEFAULT NULL,
+  `zutu` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_expand_content_movie` */
+
+DROP TABLE IF EXISTS `dc_expand_content_movie`;
+
+CREATE TABLE `dc_expand_content_movie` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `aid` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_expand_model` */
+
+DROP TABLE IF EXISTS `dc_expand_model`;
+
+CREATE TABLE `dc_expand_model` (
+  `mid` int(10) NOT NULL AUTO_INCREMENT,
+  `table` varchar(250) DEFAULT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`mid`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_expand_model_field` */
+
+DROP TABLE IF EXISTS `dc_expand_model_field`;
+
+CREATE TABLE `dc_expand_model_field` (
+  `fid` int(10) NOT NULL AUTO_INCREMENT,
+  `mid` int(10) DEFAULT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  `field` varchar(250) DEFAULT NULL,
+  `type` int(10) DEFAULT '1',
+  `property` int(10) DEFAULT NULL,
+  `len` int(10) DEFAULT NULL,
+  `decimal` int(10) DEFAULT NULL,
+  `default` varchar(250) DEFAULT NULL,
+  `sequence` int(10) DEFAULT '0',
+  `tip` varchar(250) DEFAULT NULL,
+  `must` int(10) DEFAULT '0',
+  `config` text,
+  PRIMARY KEY (`fid`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_form` */
+
+DROP TABLE IF EXISTS `dc_form`;
+
+CREATE TABLE `dc_form` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `table` varchar(20) DEFAULT NULL,
+  `display` int(10) NOT NULL DEFAULT '0',
+  `page` int(10) NOT NULL DEFAULT '10',
+  `tpl` varchar(250) DEFAULT NULL,
+  `alone_tpl` int(10) NOT NULL DEFAULT '0',
+  `order` varchar(20) DEFAULT NULL,
+  `where` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_form_data_guestbook` */
+
+DROP TABLE IF EXISTS `dc_form_data_guestbook`;
+
+CREATE TABLE `dc_form_data_guestbook` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `content` text,
+  `time` int(10) DEFAULT NULL,
+  `http` varchar(250) DEFAULT NULL,
+  `reply` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_form_field` */
+
+DROP TABLE IF EXISTS `dc_form_field`;
+
+CREATE TABLE `dc_form_field` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `fid` int(10) DEFAULT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  `field` varchar(250) DEFAULT NULL,
+  `type` int(10) DEFAULT '1',
+  `property` int(10) DEFAULT NULL,
+  `len` int(10) DEFAULT NULL,
+  `decimal` int(10) DEFAULT NULL,
+  `default` varchar(250) DEFAULT NULL,
+  `sequence` int(10) DEFAULT '0',
+  `tip` varchar(250) DEFAULT NULL,
+  `config` text,
+  `must` int(10) DEFAULT '0',
+  `admin_display` int(10) DEFAULT NULL,
+  `admin_html` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_fragment` */
+
+DROP TABLE IF EXISTS `dc_fragment`;
+
+CREATE TABLE `dc_fragment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章id',
+  `content` text,
+  `title` varchar(250) DEFAULT NULL,
+  `sign` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sign` (`sign`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='文章信息表';
+
+/*Table structure for table `dc_lang` */
+
+DROP TABLE IF EXISTS `dc_lang`;
+
+CREATE TABLE `dc_lang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `lang` varchar(255) DEFAULT NULL,
+  `protection` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `lang` (`lang`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_model` */
+
+DROP TABLE IF EXISTS `dc_model`;
+
+CREATE TABLE `dc_model` (
+  `mid` int(10) NOT NULL AUTO_INCREMENT,
+  `model` varchar(250) DEFAULT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  `admin_category` varchar(250) DEFAULT NULL,
+  `admin_content` varchar(250) DEFAULT NULL,
+  `module_category` varchar(250) DEFAULT NULL,
+  `module_content` varchar(250) DEFAULT NULL,
+  `url_category` varchar(250) DEFAULT NULL,
+  `url_category_page` varchar(250) DEFAULT NULL,
+  `url_content` varchar(250) DEFAULT NULL,
+  `url_content_page` varchar(250) DEFAULT NULL,
+  `table` text,
+  `file` text,
+  `config` text,
+  PRIMARY KEY (`mid`),
+  KEY `model` (`model`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_position` */
+
+DROP TABLE IF EXISTS `dc_position`;
+
+CREATE TABLE `dc_position` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `sequence` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_position_relation` */
+
+DROP TABLE IF EXISTS `dc_position_relation`;
+
+CREATE TABLE `dc_position_relation` (
+  `aid` int(10) NOT NULL,
+  `pid` int(10) NOT NULL,
+  KEY `aid` (`aid`),
+  KEY `pid` (`pid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_tags` */
+
+DROP TABLE IF EXISTS `dc_tags`;
+
+CREATE TABLE `dc_tags` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `click` int(10) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_tags_relation` */
+
+DROP TABLE IF EXISTS `dc_tags_relation`;
+
+CREATE TABLE `dc_tags_relation` (
+  `aid` int(10) DEFAULT NULL,
+  `tid` int(10) DEFAULT NULL,
+  KEY `aid` (`aid`),
+  KEY `tid` (`tid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_upload` */
+
+DROP TABLE IF EXISTS `dc_upload`;
+
+CREATE TABLE `dc_upload` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `file` varchar(250) DEFAULT NULL,
+  `title` varchar(250) DEFAULT NULL,
+  `folder` varchar(250) DEFAULT NULL,
+  `ext` varchar(20) DEFAULT NULL,
+  `size` int(10) DEFAULT NULL,
+  `type` varchar(250) DEFAULT NULL,
+  `time` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `type` (`type`) USING BTREE,
+  KEY `ext` (`ext`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_upload_category` */
+
+DROP TABLE IF EXISTS `dc_upload_category`;
+
+CREATE TABLE `dc_upload_category` (
+  `id` int(10) DEFAULT NULL,
+  `file_id` int(10) DEFAULT NULL,
+  KEY `id` (`id`),
+  KEY `file_id` (`file_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_upload_content` */
+
+DROP TABLE IF EXISTS `dc_upload_content`;
+
+CREATE TABLE `dc_upload_content` (
+  `id` int(10) DEFAULT NULL,
+  `file_id` int(10) DEFAULT NULL,
+  KEY `id` (`id`),
+  KEY `file_id` (`file_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_upload_form` */
+
+DROP TABLE IF EXISTS `dc_upload_form`;
+
+CREATE TABLE `dc_upload_form` (
+  `id` int(10) DEFAULT NULL,
+  `file_id` int(10) DEFAULT NULL,
+  KEY `id` (`id`),
+  KEY `file_id` (`file_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `dc_upload_plus` */
+
+DROP TABLE IF EXISTS `dc_upload_plus`;
+
+CREATE TABLE `dc_upload_plus` (
+  `id` int(10) DEFAULT NULL,
+  `file_id` int(10) DEFAULT NULL,
+  KEY `id` (`id`),
+  KEY `file_id` (`file_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/* Procedure structure for procedure `ff` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `ff` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `ff`()
+BEGIN
+    
+    
+    DECLARE i INTEGER;
+SET i=1;
+WHILE i<=10192 DO
+BEGIN
+update dc_content set urltitle =""+i+"" where  aid=i;
+SET i=i+1;
+END;
+END WHILE;
+    END */$$
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
